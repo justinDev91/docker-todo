b557aeccda9a5779a278bc036c0494015a9e04ae
justinDev91 <justin@sustainsoft.eu>
2024-02-23 13:47:51 +0100

    feat(symfony): init todo project

---
+++ src/src/Controller/HelloWordController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloWordController extends AbstractController
+{
+  #[Route('/hello', name: 'index')]
+  public function index(): Response
+  {
+    return new Response('Hello, world!');
+  }
+}
