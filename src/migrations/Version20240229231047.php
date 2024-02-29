dedeca93a67611d4ff365f23d9d705ef37d74929
justinDev91 <justin@sustainsoft.eu>
2024-03-01 00:15:52 +0100

    hotfix(php-ctype): fix extension

---
+++ src/migrations/Version20240229231047.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240229231047 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE todo DROP description');
+        $this->addSql('ALTER TABLE todo DROP status');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE todo ADD description VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE todo ADD status VARCHAR(255) NOT NULL');
+    }
+}
